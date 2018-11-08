a72ec91bbe2aaef898431f1cc438e946e484fff3
kostya150 <kostyazhivotov@gmail.com>
2018-11-09 00:07:04 +0200

    hi i am back2

--- DemoComparator.java
+++ DemoComparator.java
@@ -20,33 +20,6 @@ public class DemoComparator {
 
         capabilities.sort(new IsActiveComparator());
         System.out.println(capabilities);
-
-        /*Comparator<Capability> comparator = new Comparator<Capability>() {
-            @Override*/
-            public int compare(Capability o1, Capability o2) {
-                // если ChannelName не равно - сравниваю по нему+
-                // если равно - перехожу к fingerPrint+
-                // если fingerPrint не равно - сравниваю по нему+
-                // если равно - перехожу к DateCreated+
-                // если DateCreated не равно - сравниваю по нему
-                // если равно - объекты равны - return 0;
-                if(!o1.getChannelName().equals(o2.getChannelName())){
-                    return o1.getChannelName().compareTo(o2.getChannelName());
-                }
-                else
-                if(!o1.getFingerprint().equals(o2.getFingerprint())){
-                    return o1.getFingerprint().compareTo(o2.getFingerprint());
-                }
-                else
-                if(!o1.getDateCreated().equals(o2.getDateCreated())){
-                    return o1.getDateCreated().compareTo(o2.getDateCreated());}
-                else
-                    return 0;
-
-            }
-        //};
-
-
     }
 }
 
